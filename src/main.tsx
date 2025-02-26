fc9d9ed8d784499d3df62da57012a68e9175f3df
Alejandro Flores <alejandrofloresmtz94@gmail.com>
2025-02-26 03:32:19 -0600

    WIP: implementing mock

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@config/styles/index.css';
 import App from './App.tsx'
+import '@config/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@common": path.resolve(__dirname, "./src/common"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@states": path.resolve(__dirname, "./src/states"),
+      "@ui": path.resolve(__dirname, "./src/ui"),
+    },
+  },
 });
