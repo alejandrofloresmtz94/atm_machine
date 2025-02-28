d9f720121e4a3f5f2e873fecb11f50f66024ff69
Alejandro Flores <alejandrofloresmtz94@gmail.com>
2025-02-28 01:12:57 -0600

    WIP: Mockup progress

--- src/ui/views/ATM.tsx
+++ src/ui/views/ATM.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import credit_cards from '@common/assets/images/creditcard_sprite.png';
 import Header from "../components/Header";
+import credit_cards from '@common/assets/images/creditcard_sprite.png';
+import systems from '@common/assets/images/systems.png';
 
 const ATM: React.FC = () => {
     console.log(credit_cards);
@@ -13,18 +14,65 @@ const ATM: React.FC = () => {
                     <div className={`h-5 w-5 bg-[url('${credit_cards}')]`}></div>
                 </div>
                 <div className="flex flex-row justify-center">
-                    <div className="flex flex-col justify-end gap-10">
-                        <div>
-                            <div className="bg-custom-gray" />
+                    <div className="flex flex-col justify-end gap-10 grow pb-5">
+                        <div className="w-full h-auto flex flex-row items-center justify-end">
+                            <div className="w-8/12 h-[50px] border-y-4 border-t-light-gray border-b-philippine-gray rounded-xl bg-custom-gray" />
+                            <div className="w-2/12 h-[5px] bg-philippine-gray" />
                         </div>
-                        <div>2</div>
-                        <div>3</div>
-                        <div>4</div>
+                        <div className="w-full h-auto flex flex-row items-center justify-end">
+                            <div className="w-8/12 h-[50px] border-y-4 border-t-light-gray border-b-philippine-gray rounded-xl bg-custom-gray" />
+                            <div className="w-2/12 h-[5px] bg-philippine-gray" />
+                        </div>
+                        <div className="w-full h-auto flex flex-row items-center justify-end">
+                            <div className="w-8/12 h-[50px] border-y-4 border-t-light-gray border-b-philippine-gray rounded-xl bg-custom-gray" />
+                            <div className="w-2/12 h-[5px] bg-philippine-gray" />
+                        </div>
+                        <div className="w-full h-auto flex flex-row items-center justify-end">
+                            <div className="w-8/12 h-[50px] border-y-4 border-t-light-gray border-b-philippine-gray rounded-xl bg-custom-gray" />
+                            <div className="w-2/12 h-[5px] bg-philippine-gray" />
+                        </div>
                     </div>
-                    <div className="flex flex-col justify-between h-[500px] w-2/3 py-10 bg-iceberg border-8 border-timberwolf text-white font-main">
-                        <span className="w-100 text-center text-2xl">Welcome to the ATM!</span>
+                    <div className="flex flex-col">
+                        <div className="h-[500px] w-full bg-iceberg border-8 border-timberwolf text-white font-main grow-0 flex flex-col justify-between">
+                            <span className="text-center text-2xl">Welcome to the ATM!</span>
+                            <div className="flex flex-row justify-between">
+                                <div className="flex flex-col">
+                                    <div>1</div>
+                                    <div>2</div>
+                                    <div>3</div>
+                                    <div>4</div>
+                                </div>
+                                <div className="flex flex-col">
+                                    <div>5</div>
+                                    <div>6</div>
+                                    <div>7</div>
+                                    <div>8</div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <img src={systems} alt="" className="h-[6px] w-[54px] aspect-auto self-end mt-2" />
                     </div>
-                    <div className="flex flex-col justify-end">1</div>
+                    <div className="flex flex-col justify-end gap-10 grow pb-5">
+                        <div className="w-full h-auto flex flex-row items-center justify-start">
+                            <div className="w-2/12 h-[5px] bg-philippine-gray" />
+                            <div className="w-8/12 h-[50px] border-y-4 border-t-light-gray border-b-philippine-gray rounded-xl bg-custom-gray" />
+                        </div>
+                        <div className="w-full h-auto flex flex-row items-center justify-start">
+                            <div className="w-2/12 h-[5px] bg-philippine-gray" />
+                            <div className="w-8/12 h-[50px] border-y-4 border-t-light-gray border-b-philippine-gray rounded-xl bg-custom-gray" />
+                        </div>
+                        <div className="w-full h-auto flex flex-row items-center justify-start">
+                            <div className="w-2/12 h-[5px] bg-philippine-gray" />
+                            <div className="w-8/12 h-[50px] border-y-4 border-t-light-gray border-b-philippine-gray rounded-xl bg-custom-gray" />
+                        </div>
+                        <div className="w-full h-auto flex flex-row items-center justify-start">
+                            <div className="w-2/12 h-[5px] bg-philippine-gray" />
+                            <div className="w-8/12 h-[50px] border-y-4 border-t-light-gray border-b-philippine-gray rounded-xl bg-custom-gray" />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-row justify-end">
                 </div>
             </div>
         </>
